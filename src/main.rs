f076bda2d5e858882685d963fb33f92306fb4ebd
Frigyes06 <eszfrigyes06@gmail.com>
2022-07-08 22:24:08 +0200

    added some things

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ use net::client;
 mod net { pub mod client; }
 
 fn main() {
-    client::Connect("localhost", 9000);
+    let success = client::Connect("localhost", 9000);
+    println!("{}",success)
 }
--- src/net/client.rs
+++ src/net/client.rs
@@ -3,8 +3,8 @@ use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{Read, Write};
 use log::{info, warn};
 
-pub fn Connect(mut hostname:&str, port:u16){                //tcp connect function
-    let host = format!("{}{}{}", hostname, ":", port);   //makes string from hostname and port for the tcp function
+pub fn Connect(mut hostname:&str, port:u16) -> bool {           //tcp connect function
+    let host = format!("{}{}{}", hostname, ":", port);          //makes string from hostname and port for the tcp function
     match TcpStream::connect(&host) {
         Ok(mut stream) => {
             info!("connected to: {}", hostname);
@@ -12,6 +12,8 @@ pub fn Connect(mut hostname:&str, port:u16){                //tcp connect functi
         Err(e) => {
             println!("Failed to connect: {}", e);
             warn!("Failed to connect: {}", e);
+            return false;
         }
     }
+    return true;
 }
